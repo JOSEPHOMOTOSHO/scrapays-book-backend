93b6d51cb3259ca3facef28c0f8f5f8215089bc1
joseph omotosho <joseph.omotosho98@gmail.com>
2023-03-19 02:14:32 +0100

    feat: backend implementation for book api

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BookModule } from './book/book.module';
+import { ApolloDriver } from '@nestjs/apollo';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: `schema.gql`,
+      driver: ApolloDriver,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'data/book.db',
+      logging:true,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    BookModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
